fdb4e59b51b6f687e9cef85a676ddce73b784097
Oviya Muralidharan <113432878+oviyamuralidharan@users.noreply.github.com>
2026-07-05 22:33:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def greet():
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
     return "Hello from Jenkins CI/CD Pipeline!"
 
 if __name__ == "__main__":
-    print(greet())
+    app.run(host="0.0.0.0", port=5000)
